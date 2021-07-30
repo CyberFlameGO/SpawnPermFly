bec26b0e2cc2f37c05d37e52cf85f29b51f90ab5
CyberFlame <cyberflameu@gmail.com>
2021-07-30 12:28:51 +1200

    Update Main.java

--- src/main/java/net/cyberflame/plugin/Main.java
+++ src/main/java/net/cyberflame/plugin/Main.java
@@ -3,10 +3,12 @@ package net.cyberflame.plugin;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Main extends JavaPlugin{
+public class Main extends JavaPlugin
+{
 @Override
-public void onEnable() {
-	Bukkit.broadcastMessage("[Plugin] Plugin loaded and enabled successfully!");
-//	Bukkit.getPluginManager().registerEvents(new PlayerListener(), this);
-}
+    public void onEnable() 
+    {
+        Bukkit.broadcastMessage("[Plugin] Plugin loaded and enabled successfully!");
+        Bukkit.getPluginManager().registerEvents(new PlayerListener(), this);
+    }
 }
